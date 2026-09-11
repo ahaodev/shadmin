2701d8bf339a03f4e73666e917c215f9a2a7c073
ahaodev <52927295@qq.com>
2026-09-11 16:57:36 +0800

    refactor: name

--- frontend/src/features/system/roles/components/roles-create-dialog.tsx
+++ frontend/src/features/system/roles/components/roles-create-dialog.tsx
@@ -42,7 +42,7 @@ import { useMenuSelection } from '../hooks/use-menu-selection'
 import { type RoleFormData, useRoleForm } from '../hooks/use-role-form'
 import { MenuTreeSection } from './menu-tree-section'
 
-interface CreateRoleDialogProps {
+interface RolesCreateDialogProps {
   open: boolean
   onOpenChange: (open: boolean) => void
   role?: Role | null // Optional role for edit mode
@@ -52,7 +52,7 @@ export function RolesCreateDialog({
   open,
   onOpenChange,
   role,
-}: CreateRoleDialogProps) {
+}: RolesCreateDialogProps) {
   const isEditMode = !!role
   const queryClient = useQueryClient()
 
